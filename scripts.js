9707c6403e9b5b5af3f0d45900b8f02e6fd429e9
Matan-Abir <132847972+Matan-Abir@users.noreply.github.com>
2023-05-11 20:49:41 +0300

    Section placeholders, title design change
    Moved the name/contact/description to the top middle and enlarged the name.
    Added the education/experience/flavor sections.
    
    todo; counter-balance empty right side. mobile version

--- scripts.js
+++ scripts.js
@@ -1,13 +1,3 @@
-// Fix this HTML first
-
-document.querySelectorAll('.section-title').forEach(title => {
-    title.addEventListener('click', () => {
-        const content = title.nextElementSibling;
-        content.style.display = content.style.display === 'none' ? 'block' : 'none';
-    });
-});
-
-
 // GitHub repos
 const githubUsername = "Matan-Abir";
 
@@ -29,3 +19,33 @@ fetch(`https://api.github.com/users/${githubUsername}/repos`)
     .catch((error) => {
         console.error("Error fetching GitHub repos:", error);
     });
+
+
+// Tabs
+let acc = document.getElementsByClassName("section-btn");
+let i;
+
+window.onload = function() {
+    let workExperienceContent = document.getElementById("work-experience-content");
+    workExperienceContent.style.maxHeight = workExperienceContent.scrollHeight + "px";
+}
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        // close all sections
+        for (let j = 0; j < acc.length; j++) {
+            if (acc[j] === this) continue;
+            acc[j].classList.remove("active");
+            let content = acc[j].nextElementSibling;
+            content.style.maxHeight = null;
+        }
+        // open or close clicked section
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
